d9d2a3f68793f557c0bc44644d997f10a0e22be9
Sarah Anja <100360306+macobac@users.noreply.github.com>
2023-04-23 12:34:43 +0200

    combine btn and details page

---
+++ details.js
@@ -0,0 +1,40 @@
+//const mainAPI = "http://museum.local/wp-json";
+const productsAPI = "https://museum.local/wp-json/wc/store/products";
+const cKey = "ck_cc425f48471600ede2440a3806a96202ef56bb12";
+const cSecret = "cs_ac9df972f37d6c360f556944d743e44fced33d0a";
+const detailsDiv = document.querySelector("#details-div");
+
+const urlParams = new URLSearchParams(window.location.search);
+const prodIndex = parseInt(urlParams.get('product'));
+
+fetch(productsAPI, {
+  headers: {
+    'Authorization': 'Basic ' + btoa(cKey + ':' + cSecret),
+    'Content-Type': 'application/json'
+  }
+})
+  .then(response => response.json())
+  .then(products => {
+    const product = products[prodIndex];
+
+    // Create HTML for product details
+    const detailsDiv = document.getElementById('details-div');
+    detailsDiv.innerHTML = `
+      <div class="left-wrap">
+        <img src="${product.images[0].src}" alt="${product.images[0].alt}" id="prod-img">
+      </div>
+      <div class="right-wrap">
+        <h2 id="prod-titel">${product.name}</h2>
+        <p id="prod-desc">${product.description}</p>
+        <p id="prod-price">${product.prices.price/100} kr</p>
+        <p id="prod-sale-price">${product.prices.sale_price/100} kr</p>
+        <p id="prod-avg-rating">${product.average_rating}</p>
+        <p id="prod-review-count">${product.review_count}</p>
+        <p id="prod-categories">${product.categories.map(category => category.name).join(', ')}</p>
+        <p id="prod-in-stock">${product.stock_quantity}</p>
+      </div>
+    `;
+  })
+  .catch(error => {
+    console.error('Error fetching product:', error);
+  });
--- shop.js
+++ shop.js
@@ -4,31 +4,35 @@ const cKey = "ck_cc425f48471600ede2440a3806a96202ef56bb12";
 const cSecret = "cs_ac9df972f37d6c360f556944d743e44fced33d0a";
 const prodDiv = document.querySelector("#product-content");
 
-fetch(productsAPI, {
-  headers: {
-    'Authorization': 'Basic ' + btoa(cKey + ':' + cSecret),
-    'Content-Type': 'application/json'
-  }
-})
 
+  fetch(productsAPI, {
+    headers: {
+      "Authorization": "Basic " + btoa(cKey + ":" + cSecret),
+      "Content-Type": "application/json"
+    }
+  })
   .then(response => response.json())
   .then(products => {
-    products.forEach(products =>
+    products.forEach((product, index) => {
       prodDiv.innerHTML += `
       <div class="left-wrap">
-        <img src="${products.images[0].src}" alt="${products.images[0].alt}" id="prod-img">
+        <img src="${product.images[0].src}" alt="${product.images[0].alt}" id="prod-img">
       </div>
       <div class="right-wrap">
-        <h2 id="prod-titel">${products.name}</h2>
-        <p id="prod-desc">${products.description}</p>
-        <p id="prod-price">${products.prices.price/100} kr</p>
-        <button id="add-to-cart">Add to cart</button>
+        <h2 id="prod-titel">${product.name}</h2>
+        <p id="prod-desc">${product.description}</p>
+        <p id="prod-price">${product.prices.price / 100} kr</p>
+        <button class="add-to-cart">Add to cart</button>
       </div>
-      `
-    )
+      `;
+    });
+    const cartBtns = document.querySelectorAll(".add-to-cart");
+    cartBtns.forEach((button, index) => {
+      button.addEventListener("click", () => {
+        window.location.href = `details.html?product=${index}`;
+      })
+    });
   })
-
   .catch(error => {
-    console.error('Error fetching products:', error);
+    console.error("Error fetching products:", error);
   });
-
